1a1d62eee437e8974cfdb9b7e7007fd4b4abd678
wubin2 <wubin@coolsong.com>
2021-05-22 00:34:16 +0800

    v1.2 upload

---
+++ custom_components/pushplus/notify.py
@@ -0,0 +1,60 @@
+"""
+PushPlus V1.2 For HomeAssistant by Tristan
+
+"""
+import logging
+#from fake_useragent import UserAgent
+import datetime
+import requests
+import voluptuous as vol
+
+from homeassistant.components.notify import (
+    ATTR_MESSAGE, ATTR_TITLE, ATTR_DATA, ATTR_TARGET, PLATFORM_SCHEMA, BaseNotificationService)
+import homeassistant.helpers.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+_RESOURCE = 'http://www.pushplus.plus/send'
+
+CONF_TOKEN = 'token'
+CONF_TOPIC= 'topic'
+CONF_TEMPLATE= 'template'
+def get_service(hass, config, discovery_info=None):
+    """Get the PushPlus notification service."""
+    token = config.get(CONF_TOKEN )
+    topic = config.get(CONF_TOPIC )
+    template = config.get(CONF_TEMPLATE )
+    return PushPlusNotificationService(hass, token, topic, template)
+
+
+class PushPlusNotificationService(BaseNotificationService):
+    """Implementation of the notification service for SimplePush."""
+
+    def __init__(self, hass, token, topic, template):
+        """Initialize the service."""
+        _LOGGER.debug("INIT message")
+        self._token = token
+        self._topic = topic
+        self._template = template
+
+    def send_message(self, message='', **kwargs):
+        """Send a message to a user."""
+        url = '{}?token={}&topic={}&template={}&'.format(_RESOURCE, self._token, self._topic, self._template)
+        title = kwargs.get(ATTR_TITLE)
+        if title:
+           timestp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+           sendmessage = '{} {}'.format(timestp, message)
+           payload = {'title': title, 'content': sendmessage}
+           #ua = UserAgent()
+           #header = {'user-agent': ua.random }
+           response = requests.get(url,params = payload)
+           _LOGGER.debug("sneding out message")
+		
+           if response.status_code != 200:
+              obj = response.json()
+              error_message = obj['error']['message']
+              error_code = obj['error']['code']
+              _LOGGER.error(
+                   "Error %s : %s (Code %s)", resp.status_code, error_message,
+                   error_code)
+        else:
+           _LOGGER.error("Title can NOT be null")
